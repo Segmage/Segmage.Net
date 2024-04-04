ee90769b16077f0078e54a4cc549db99ff730a3e
İlter Yıldız <ilter.yildiz@arge24.com.tr>
2024-04-04 13:16:56 +0300

    bugfix App options configuration load

--- examples/Segmage.WebApp.Example/Program.cs
+++ examples/Segmage.WebApp.Example/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSegmage("TEST_INSTANCE-1", async options =>
 {
-    options=await options.FromFileAsync("test-1-conf.json");
+    options=await AppOptions.FromFileAsync("test-1-conf.json");
 });
 
 var app = builder.Build();
